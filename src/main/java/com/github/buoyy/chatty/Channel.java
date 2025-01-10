c04b120a1027effac9ba92adb4ee120dafae1457
Buoyy <saarthak0902@gmail.com>
2025-01-10 21:20:56 +0530

    Some small changes

--- src/main/java/com/github/buoyy/chatty/Channel.java
+++ src/main/java/com/github/buoyy/chatty/Channel.java
@@ -6,27 +6,33 @@ import org.bukkit.entity.Player;
 public class Channel {
     //UUID id;
     ArrayList<Player> players;
+    ArrayList<Player> admins;
+    String name;
     
     // This constructor is for when a player runs the command
     // i.e., it automatically sets the player as channel OP
     // TODO: Add more useful constructors
-    public Channel(/*UUID id*/) {
-        // Create some random ID and assign it for this channel
+    public Channel(String name) {
+        // String references are TEMPORARY.
         // The 0th player *must* be the OP
         // this is a temp. soln
         // TODO: add an OPs list for multi operator channel (useful for keeping mods and stuff)
+        this.name = name;
+        players = new ArrayList<>();
+        admins = new ArrayList<>();
+        admins.add(players.getFirst());
     }
     
-    public Player getOP() {
-        return players.get(0);
+    public ArrayList<Player> getAdmins() {
+        return admins;
     }
     public void setOP(Player player) {
         players.set(0, player);
     }
-    public addPlayer(Player player) {
+    public void addPlayer(Player player) {
         players.add(player);
     }
-    public ArrayList getPlayers() {
+    public ArrayList<Player> getPlayers() {
         return players;
     }
 }
